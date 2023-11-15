54a15eeca6e359ae533b03fe6c6a1c6c2625be8e
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2023-11-16 00:14:55 +0100

    update for icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
+import brokenclouds from './assets/icons/brokenclouds.png'
+import clearday from './assets/icons/clearday.png'
+import clearnight from './assets/icons/clearnight.png'
+import clouds from './assets/icons/clouds.png'
+import fewcloudsday from './assets/icons/fewcloudsday.png'
+import fewcloudsnight from './assets/icons/fewcloudsnight.png'
+import mist from './assets/icons/mist.png'
+import rainday from './assets/icons/rainday.png'
+import rainnight from './assets/icons/rainnight.png'
+import showerrain from './assets/icons/showerrain.png'
+import snow from './assets/icons/snow.png'
+import thunderstorm from './assets/icons/thunderstorm.png'
+
+const weatherIcons = {
+  'clear sky': { day: clearday, night: clearnight },
+  'few clouds': { day: fewcloudsday, night: fewcloudsnight },
+  'scattered clouds': { day: clouds, night: clouds },
+  'broken clouds': { day: brokenclouds, night: brokenclouds },
+  'mist': { day: mist, night: mist },
+  'light rain': { day: rainday, night: rainnight },
+  'shower rain': { day: showerrain, night: showerrain },
+  'snow': { day: snow, night: snow },
+  'thunderstorm': { day: thunderstorm, night: thunderstorm },
+};
+
 
 function App() {
   const [data, setData] = useState({})
@@ -24,6 +49,16 @@ function App() {
       </div>
       <div className="container">
         <div className="top">
+          <div className="icon">
+            {data.weather && data.weather[0].icon && weatherIcons[data.weather[0].description] && (
+              <img
+                src={weatherIcons[data.weather[0].icon.slice(-1) === 'd' ? 'clear sky' : data.weather[0].description].day}
+                alt={data.weather[0].description}
+              />
+
+            )}
+
+          </div>
           <div className="location">
             <p>{data.name}</p>
           </div>
@@ -35,7 +70,7 @@ function App() {
           </div>
         </div>
 
-        {data.name != undefined &&
+        {data.name != undefined && (
           <div className="bottom">
             <div className="feels">
               {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°</p> : null}
@@ -50,8 +85,7 @@ function App() {
               <p>Wind Speed</p>
             </div>
           </div>
-        }
-
+        )}
       </div>
     </div>
   );
